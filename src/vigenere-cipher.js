a477165c9db564c1b313924415deb63d3f08aa62
TatsianaSauko <miljanzowa@mail.ru>
2023-04-14 20:35:31 +0300

    feat: added solutions vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,60 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+
+const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(value = true) {
+    this.value = value
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (message === undefined || key === undefined || arguments.length < 2) {
+      throw new Error('Incorrect arguments!')
+    }
+    message = message.toUpperCase().split('')
+    key = key.repeat(Math.ceil(message.length/key.length)).toUpperCase().split('').slice(0, message.length)
+    let result = []
+    for (let i = 0; i < message.length; i++) {
+      if (!(alphabet.includes(message[i]))) {
+        result.push(message[i])
+        key.splice(i, 0, message[i])
+        // console.log(key)
+      } else {
+        let index = alphabet.indexOf(message[i]) + alphabet.indexOf(key[i])
+        if (index > 25) {
+          result.push(alphabet[index % 26])
+        } else {
+          result.push(alphabet[index])
+        }
+
+      }
+    }
+    return this.value === true ? result.join('') : result.reverse().join('')
+  }
+
+  decrypt(message, key) {
+    if (message === undefined || key === undefined || arguments.length < 2) {
+      throw new Error('Incorrect arguments!')
+    }
+    message = message.toUpperCase().split('')
+    key = key.repeat(Math.ceil(message.length/key.length)).toUpperCase().split('').slice(0, message.length)
+    let result = []
+
+
+    for (let i = 0; i < message.length; i++) {
+      if (!(alphabet.includes(message[i]))) {
+        result.push(message[i])
+        key.splice(i, 0, message[i])
+      } else {
+        let index = alphabet.indexOf(message[i]) - alphabet.indexOf(key[i])
+        if (index < 0) {
+          result.push(alphabet[25 + index + 1])
+        } else {
+          result.push(alphabet[index])
+        }
+      }
+    }
+    return this.value === true ? result.join('') : result.reverse().join('')
   }
 }
 
